b5e300b79c17ac93dc864e685c430b1d6c7cb9d8
Brian Dombrowski <bdombro@gmail.com>
2019-08-11 20:55:20 -0400

    Unimodule working

---
+++ dist/routing.web.js
@@ -0,0 +1,1 @@
+Object.defineProperty(exports,"__esModule",{value:true});Object.defineProperty(exports,"matchPath",{enumerable:true,get:function get(){return _reactRouterDom.matchPath;}});Object.defineProperty(exports,"withRouter",{enumerable:true,get:function get(){return _reactRouterDom.withRouter;}});var _reactRouterDom=require("react-router-dom");
